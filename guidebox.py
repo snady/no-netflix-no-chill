0b37d8fefd911c3c54067c912ff75aec4909993c
freepina <freepina@gmail.com>
2015-11-11 20:45:12 -0500

    weather and zip code finding system added

--- guidebox.py
+++ guidebox.py
@@ -37,3 +37,25 @@ for i in r:
         print t['theatre']['name'], 
         print t['dateTime'].split('T') 
     print '\n'
+
+################### OpenWeather #####################
+
+def weather(zipcode):
+    url = '''http://api.openweathermap.org/data/2.5/weather?zip=%s,us&appid=2de143494c0b295cca9337e1e96b00e0&units=metric''' #superior metric unit
+    url = url%(zipcode)
+    request = urllib2.urlopen(url)
+    result = request.read()
+    r = json.loads(result)
+    return r['main']['temp']
+    
+################### Zippopotamus #####################
+
+def zipcode(state,city):
+    url = '''http://api.zippopotam.us/us/%s/%s'''
+    url = url%(state,city)
+    request = urllib2.urlopen(url)
+    result = request.read()
+    r = json.loads(result)
+    if r == {}:
+        return "error"
+    return r['places']
