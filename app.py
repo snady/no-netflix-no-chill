923ef8e69839d22a1bdbd89f946f41dd4a83a41e
Sandy Fang <sfang2016@gmail.com>
2015-11-15 19:58:01 -0500

    filtering and spooky stuff

--- app.py
+++ app.py
@@ -6,15 +6,21 @@ app = Flask(__name__)
 
 ######################## globals ##############################
 
-thresh = 0
+thresh = 15
 temp = -273 #temperature in celcius. If abs 0, then err
 zipc = ""
+genre = ""
+rating = ""
 
 @app.route("/", methods = ['GET','POST'])
 def index():
         if request.method == 'POST':
                 global zipc
                 zipc = request.form['zipcode']
+                global genre
+                genre = request.form['genre']
+                global rating
+                rating = request.form['rating']
                 global temp
                 temp = api.weather(zipc)
                 if temp >= thresh:
@@ -26,12 +32,20 @@ def index():
 @app.route("/chill")
 def chill():
         movies = api.amazon()
+        if genre != 'no':
+                movies = filterGenre(movies, genre)
+        if rating != 'no':
+                movies = filterRating(movies, rating)
         return render_template("chill.html", movies=movies, temp=temp)
 
 @app.route("/nochill")
 def nochill():
         print zipc
         movies = api.showtimes(zipc)
+        if genre != 'no':
+                movies = filterGenre(movies, genre)
+        if rating != 'no':
+                movies = filterRating(movies, rating)
         return render_template("nochill.html", movies=movies, temp=temp)
 
 def filterGenre(json, genre):
